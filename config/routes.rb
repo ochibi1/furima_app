ce2ff927c3dc99d71b5daf3f9b37938c6d88c6e8
ochibi1 <lapotit0122@gmail.com>
2020-07-22 22:25:38 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
+  resources: users
+  resources: deliver_address, only: [:create, :update, :edit]
+  resources: creditcards, only: [:create, :update, :edit]
+  resources: photos, only: [:create]
+  resources: brands, only: [:create]
+  resources: products do 
+    resources: comments
+    resources: favorites
+  end
 end
