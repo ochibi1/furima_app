a87b7a61e458465e53ef9f0d4011a800f11ef7c9
ike-re <67263156+ike-re@users.noreply.github.com>
2020-07-23 20:27:16 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#indexxxxxx'
+  root 'items#index'
   resources: users
   resources: deliver_addresses, only: [:create, :update, :edit]
   resources: creditcards, only: [:create, :update, :edit]
