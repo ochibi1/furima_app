eac9aadd506a5a170075d2a0f72aa42d493ee55b
ochibi1 <lapotit0122@gmail.com>
2020-07-23 09:01:22 +0900

    deleting favorites &  photos action,  fixing to plural (deliver_address)

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources: users
-  resources: deliver_address, only: [:create, :update, :edit]
+  resources: deliver_addresses, only: [:create, :update, :edit]
   resources: creditcards, only: [:create, :update, :edit]
-  resources: photos, only: [:create]
   resources: brands, only: [:create]
   resources: products do 
     resources: comments
-    resources: favorites
   end
 end
