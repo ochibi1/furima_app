d73d6458b3d8efb16a6ff6710c4187be6176a1ec
disknkmr <mercidisk@gmail.com>
2020-07-23 18:14:38 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_07_22_101453) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.date "birth_year", null: false
-    t.date "birth_month", null: false
-    t.date "birth_day", null: false
-    t.text "introduction"
-    t.string "icon"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
