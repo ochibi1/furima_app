883656b4f50dd771643c0a4a6ef7df56890211dd
ochibi1 <lapotit0122@gmail.com>
2020-07-23 23:28:35 +0900

    setting config/application.rb for rspec files

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,11 @@ module FurimaApp
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec, 
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            routing_specs: false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
