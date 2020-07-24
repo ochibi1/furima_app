c7468c23c8ac3184cc92f1c92c99153102a45a9e
ochibi1 <lapotit0122@gmail.com>
2020-07-24 19:26:26 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   describe "#create" do
 
-    it "is valid with a nickname, email, password, password_confirmation, first_name, family_name, first_name_kana, family_name_kana, birth_year, birth_month and birth_day" do
+    it "is valid with a nickname, email, password, password_confirmation, first_name, family_name, first_name_kana, family_name_kana, birth_date" do
       user = build(:user)
       expect(user).to be_valid
     end
@@ -16,6 +16,12 @@ describe User do
 
     it "is invalid without an email" do
       user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "is invalid with an email that without domain" do
+      user = build(:user, email: user)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
