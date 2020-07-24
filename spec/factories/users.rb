80118d0d1757c9068533a55594a417f51d50af73
ochibi1 <lapotit0122@gmail.com>
2020-07-24 17:39:58 +0900

    factorybot user defenition

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,16 +2,16 @@ FactoryBot.define do
   
   factory :user do
     nickname                { "tarou" }
-    email                   { "user.email" }
+    email                   { "user@gmail.com" }
     password                { "2222222" }
     password_confirmation   { "2222222" }
     first_name              { "taro" }  
     family_name             { "tanaka" }
     first_name_kana         { "taro" }
     family_name_kana        { "tanaka" }
-    birth_year              { "1990" }  
-    birth_month             { "01" }
-    birth_day               { "01" }
+    birth_year              { '1995-01-01' }  
+    birth_month             { '1995-01-01' }
+    birth_day               { '1995-01-01' }
   end
   
 end
