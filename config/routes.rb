ea49b5c14d16fff8fc240e69c12194bf766a4e7b
t_omo_o_03 <shishimarureo@gmail.com>
2020-07-25 21:16:18 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'deliver_addresses', to: 'users/registrations#new_deliver_address'
+    post 'deliver_addresses', to: 'users/registrations#create_deliver_address'
+  end
   root 'items#index'
   resources :users
-  resources :deliver_addresses, only: [:create, :update, :edit]
+  # resources :deliver_addresses, only: [:create, :update, :edit]
   resources :creditcards, only: [:create, :update, :edit]
   resources :brands, only: [:create]
   resources :products do 
