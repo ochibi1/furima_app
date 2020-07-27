a879c8cb10350e3b9fa21f6b3d9857f70aa95fc8
ochibi1 <lapotit0122@gmail.com>
2020-07-27 16:12:13 +0900

    phone_number test code

--- spec/models/deliver_address_spec.rb
+++ spec/models/deliver_address_spec.rb
@@ -56,5 +56,27 @@ describe DeliverAddress do
       expect(deliver_address.errors[:house_number]).to include("を入力してください")
     end
 
+    it "is valid with phone_number that has 11 numbers" do
+      deliver_address = build(:deliver_address, phone_number: "09012345678")
+      expect(deliver_address).to be_valid
+    end
+
+    it "is valid with phone_number that has 10 numbers" do
+      deliver_address = build(:deliver_address, phone_number: "0987654321")
+      expect(deliver_address).to be_valid
+    end
+
+    it "is invalid with phone_number that has less than 9 numbers" do
+      deliver_address = build(:deliver_address, phone_number: "098765432")
+      deliver_address.valid?
+      expect(deliver_address.errors[:phone_number]).to include("は不正な値です")
+    end
+
+    it "is invalid with phone_number that has more than 12 numbers" do
+      deliver_address = build(:deliver_address, phone_number: "009012345678")
+      deliver_address.valid?
+      expect(deliver_address.errors[:phone_number]).to include("は不正な値です")
+    end
+
   end
 end
