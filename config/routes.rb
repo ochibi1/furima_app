bb0a63caf0e9cdbb17924792ec1c642c0912251d
ochibi1 <lapotit0122@gmail.com>
2020-07-29 16:49:15 +0900

    fix credit_cards actions

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :users
   # resources :deliver_addresses, only: [:create, :update, :edit]
-  resources :creditcards, only: [:create, :update, :edit]
+  resources :credit_cards
   resources :brands, only: [:create]
   resources :products do 
     resources :comments
