79a1ec74ef308924a76bf6928f90b225e12ffc4c
t_omo_o_03 <shishimarureo@gmail.com>
2020-07-30 01:40:53 +0900

    Update photo.js

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,15 +1,17 @@
 $(function() {
-  $('input[type=file]').after('<div class="fileimg"><div/>');
+  $('input[type=file]').after('<div class="fileimg" style="display: flex;flex-wrap: wrap;"><div/>');
   let img_count = 1;
+
   $('input[type=file]').change(function() {
     let file = $(this).prop('files');
-
+    console.log(file);
     $(file).each(function(i) {
       if (img_count > 10) {
         return false;
       }
     
       if (! file[i].type.match('image.*')) {
+        alert('この形式のファイルはアップロードできません');
         $(this).val('');
         $('.fileimg').html('');
         return;
@@ -18,7 +20,9 @@ $(function() {
       let reader = new FileReader();
       reader.onload = function() {
         let img_src = $('<img style="width: 7rem;height: 7rem;">').attr('src', reader.result);
-        $('.fileimg').append(img_src);
+        let box = $('<div style="margin-bottom: 0.3rem;background: whitesmoke;display: flex;flex-direction: column;justify-content: center;align-items: center;"></div>');
+        let box2 =  box.append(img_src).append($('<p class="deletefile" style="margin: 0;padding: 0.2rem;font-size: 0.9rem;color: #4897d8;font-weight: normal;cursor: pointer;">削除</p>'));
+        $('.fileimg').append(box2);
       }
       reader.readAsDataURL(file[i]);
 
@@ -26,4 +30,8 @@ $(function() {
       console.log(img_count)
     });
   });
+
+  // $('.deletefile').on('click', function() {
+
+  // });
 });
