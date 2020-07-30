70b42c76271fb6de06512c649cbd2f399c6d646c
ruka-tamaki <ruka112626@gmail.com>
2020-07-30 15:46:47 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2020_07_29_071020) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "first_name", null: false
     t.string "family_name", null: false
     t.string "first_name_kana", null: false
