d65d4e2d080572339a3060a3cfa01ca6f915a52e
ochibi1 <lapotit0122@gmail.com>
2020-07-31 15:11:43 +0900

    pull upstream master photo table

--- db/migrate/20200731040948_create_photos.rb
+++ db/migrate/20200731040948_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.string :image, null:false
+      t.string :image,       null: false
       t.references :product, foreign_key: true
       t.timestamps
     end
