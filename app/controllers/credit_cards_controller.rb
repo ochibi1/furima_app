4c10e91b470a5497e8ca05993a3171c9f9cd9a24
t_omo_o_03 <shishimarureo@gmail.com>
2020-07-31 13:54:53 +0900

    Fix routes file_name and directory_name

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,5 @@
 class CreditCardsController < ApplicationController
+  before_action :authenticate_user!
 
   require "payjp"
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  def index 
+  before_action :authenticate_user!, except: :new
+
+  def new
   end
 
   def show 
   end
 
   def destroy
+  end
+
+  def logout
   end
 end
