a46041976308c97107085945181eb71855f31c5d
t_omo_o_03 <shishimarureo@gmail.com>
2020-08-02 02:42:58 +0900

    Finish basic upload-photos

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -19,7 +19,7 @@ $(function() {
       reader.onload = function() {
         let img_src = $('<img style="width: 7rem;height: 7rem;">').attr('src', reader.result);
         let box = $(`<div class="fileinput${img_index - 1}" style="margin-bottom: 0.3rem;background: whitesmoke;display: flex;flex-direction: column;justify-content: center;align-items: center;"></div>`);
-        let box2 =  box.append(img_src).append('<p class="deletefile" style="margin: 0;padding: 0.2rem;font-size: 0.9rem;color: #4897d8;font-weight: normal;cursor: pointer;">削除</p>');
+        let box2 =  box.append(img_src).append('<p class="deletefile" style="margin: 0;padding: 0.2rem;color: #4897d8;font-size: 0.9rem;font-weight: normal;cursor: pointer;">削除</p>');
         $('.fileimg').append(box2);
       }
       reader.readAsDataURL(file);
