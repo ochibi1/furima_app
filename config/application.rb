ee26548a3374df44c966c03ba70fa55f333ab439
t_omo_o_03 <shishimarureo@gmail.com>
2020-08-02 23:30:38 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module FurimaApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
