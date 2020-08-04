1ebbe8119f1558e75a8d1b882ba5101bfb7f3666
disknkmr <mercidisk@gmail.com>
2020-08-04 19:17:52 +0900

    Update category_spec.rb

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
+describe Category do
+  describe "#create" do
 
+    it "is invalid without a name" do
+      category = build(:category, name: "")
+      category.valid?
+      expect(category.errors[:name]).to include("を入力してください")
+    end
+
+  end
 end
