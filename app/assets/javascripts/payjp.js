47820a83da451fd5b6a9f2cf56fafd503118635b
ochibi1 <lapotit0122@gmail.com>
2020-08-06 00:23:31 +0900

    Update payjp.js

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -7,20 +7,20 @@ document.addEventListener(
         e.preventDefault();
         let card = {
           number: document.getElementById("card_number").value,
-          cvc: document.getElementById("cvc").value,
           exp_month: document.getElementById("exp_month").value,
-          exp_year: document.getElementById("exp_year").value
+          exp_year: document.getElementById("exp_year").value,
+          cvc: document.getElementById("cvc").value
         };
         Payjp.createToken(card, (status, response) => {
           if (status === 200) {
             $("#card_number").removeAttr("name");
-            $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
             $("#exp_year").removeAttr("name");
+            $("#cvc").removeAttr("name");
             $("#card_token").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
-            );
-            document.inputForm.submit();
+              );
+            document.forms["card-token"].submit();
             alert("登録が完了しました");
           } else {
             alert("カード情報が正しくありません。");
@@ -31,5 +31,3 @@ document.addEventListener(
   },
   false
 );
-
-// 4242424242424242
