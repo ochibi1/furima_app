2a21409767547365e7b3862f01f21283a21ae471
disknkmr <mercidisk@gmail.com>
2020-08-05 16:34:12 +0900

    update

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -5,5 +5,10 @@ describe Brand do
       brand = build(:brand)
       expect(brand).to be_valid
     end
+    it "is invalid without a name" do
+      brand = build(:brand, name: "")
+      brand.valid?
+      expect(brand.errors[:name]).to include( "を入力してください" )
+    end
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe Category do
   describe "#create" do
-
+    it "is valid with a name" do
+      category = build(:category)
+      expect(category).to be_valid
+    end
     it "is invalid without a name" do
       category = build(:category, name: nil)
       category.valid?
