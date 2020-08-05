4a55def201e7c6f5300be55fa866c183f27b6145
ochibi1 <lapotit0122@gmail.com>
2020-08-06 00:34:15 +0900

    add instance & column

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -25,7 +25,7 @@ class CreditCardsController < ApplicationController
       )
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to  credit_card_path
+        redirect_to  credit_card_path(@card)
       else
         render :create
       end
