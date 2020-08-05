f7584b42d04e8affacc9168de92ace4343b23da8
ochibi1 <lapotit0122@gmail.com>
2020-08-05 17:29:37 +0900

    other fixes(not directly related to 購入機能）

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,20 @@ Rails.application.routes.draw do
   get 'users/logout'
   get 'products/search_category_children'
   get 'products/search_category_grandchildren'
+  get 'products/purchase'
   resources :users
   resources :credit_cards do
     collection do 
-      post 'show', to: 'credit_cards#show'
       post 'create', to: 'credit_cards#create'
+      post 'show', to: 'credit_cards#show'
       post 'delete', to: 'credit_cards#delete'
     end
   end
   resources :brands, only: [:create]
   resources :products do 
+    collection do
+      get  'purchase/:id' => 'products#purchase', as: 'purchase'
+    end
     resources :comments
   end
 end
