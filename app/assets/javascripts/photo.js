b61811b3aeaa6974e7f4749c258b61c30e030294
disknkmr <mercidisk@gmail.com>
2020-08-07 17:10:22 +0900

    Update photo.js

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,45 +1,76 @@
-$(function() {
+$(function () {
+  img_count =
+    0 +
+    $(".ProductsNew__main__form__group__field__file-input__prevbox__image")
+      .length;
+  img_index = 0;
+  $(".file-label").on("click", function () {
+    $(".fileinput").change(function () {
+      let label = $("#files").find("label");
+      let file = $(this).prop("files")[0];
 
-  img_count = 0 + $('.ProductsNew__main__form__group__field__file-input__prevbox__image').length
-  img_index = 0
-  $('.file-label').on('click', function() {
-    $('.fileinput').change(function() {
-      let label = $('#files').find('label')
-      let file = $(this).prop('files')[0];
-    
-      if (! file.type.match('image.*')) {
-        alert('この形式のファイルはアップロードできません');
+      if (!file.type.match("image.*")) {
+        alert("この形式のファイルはアップロードできません");
         $(this).remove();
-        label.attr('for', `filearea${img_index + 1}`);
-        label.before(`<input name="product[photos_attributes][${img_index + 1}][image]" id="filearea${img_index + 1}" type="file" class="fileinput fileinput${img_index + 1}" style="display: none;">`);
-        img_index += 1
+        label.attr("for", `filearea${img_index + 1}`);
+        label.before(
+          `<input name="product[photos_attributes][${
+            img_index + 1
+          }][image]" id="filearea${
+            img_index + 1
+          }" type="file" class="fileinput fileinput${
+            img_index + 1
+          }" style="display: none;">`
+        );
+        img_index += 1;
         return;
       } else {
         let reader = new FileReader();
-        reader.onload = function() {
-          let img_src = $('<img style="width: 7rem;height: 7rem;">').attr('src', reader.result);
-          let box = $(`<div class="fileinput${img_index - 1}" style="margin-bottom: 0.3rem;background: whitesmoke;display: flex;flex-direction: column;justify-content: center;align-items: center;"></div>`);
-          let box2 =  box.append(img_src).append('<p class="deletefile" style="margin: 0;padding: 0.2rem;color: #4897d8;font-size: 0.9rem;font-weight: normal;cursor: pointer;">削除</p>');
-          $('.ProductsNew__main__form__group__field__file-input__prevbox').append(box2);
-        }
+        reader.onload = function () {
+          let img_src = $('<img style="width: 7rem;height: 7rem;">').attr(
+            "src",
+            reader.result
+          );
+          let box = $(
+            `<div class="fileinput${
+              img_index - 1
+            }" style="margin-bottom: 0.3rem;background: whitesmoke;display: flex;flex-direction: column;justify-content: center;align-items: center;"></div>`
+          );
+          let box2 = box
+            .append(img_src)
+            .append(
+              '<p class="deletefile" style="margin: 0;padding: 0.2rem;color: #4897d8;font-size: 0.9rem;font-weight: normal;cursor: pointer;">削除</p>'
+            );
+          $(
+            ".ProductsNew__main__form__group__field__file-input__prevbox"
+          ).append(box2);
+        };
         reader.readAsDataURL(file);
-        label.attr('for', `filearea${img_index + 1}`);
-        label.before(`<input name="product[photos_attributes][${img_index + 1}][image]" id="filearea${img_index + 1}" type="file" class="fileinput fileinput${img_index + 1}" style="display: none;">`);
-        img_count += 1
-        img_index += 1
+        label.attr("for", `filearea${img_index + 1}`);
+        label.before(
+          `<input name="product[photos_attributes][${
+            img_index + 1
+          }][image]" id="filearea${
+            img_index + 1
+          }" type="file" class="fileinput fileinput${
+            img_index + 1
+          }" style="display: none;">`
+        );
+        img_count += 1;
+        img_index += 1;
         if (img_count == 10) {
-          label.attr('style', 'display: none;');
+          label.attr("style", "display: none;");
         }
       }
     });
   });
 
-  $(document).on('click', '.deletefile', function() {
-    boxClass = $(this).parent().prop('class');
+  $(document).on("click", ".deletefile", function () {
+    boxClass = $(this).parent().prop("class");
     $(`.${boxClass}`).remove();
     if (img_count == 10) {
-      $('.file-label').attr('style', 'display: flex;')
+      $(".file-label").attr("style", "display: flex;");
     }
-    img_count -= 1
+    img_count -= 1;
   });
 });
