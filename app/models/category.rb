70104d776a48068e66aa75d37b920764437aea5e
t_omo_o_03 <shishimarureo@gmail.com>
2020-08-10 17:36:16 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   validates :name, presence: true
   has_many :products, dependent: :destroy
   has_ancestry
+
+  def self.set_parents
+    Category.where(ancestry: nil).order(id: "ASC")
+  end
 end
